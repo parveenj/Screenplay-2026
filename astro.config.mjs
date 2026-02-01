cae81f2f0cade6f4034f2a99497d12163a920650
parveenj <parveenj@gmail.com>
2026-02-01 02:37:47 -0800

    Trying to upload to GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://parveenj.github.io",
+  site: "https://parveenj.github.io/Screenplay-2026/",
   base: "/Screenplay-2026/",
   vite: {
     plugins: [tailwindcss()],
